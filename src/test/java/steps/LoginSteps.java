3d5ef7c972dc4a62681b911da8cd71d3d02359f3
sohai <SyntaxTechnologies>
2022-08-14 21:03:41 +0530

    lc1408

--- src/test/java/steps/LoginSteps.java
+++ src/test/java/steps/LoginSteps.java
@@ -8,17 +8,19 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import utils.CommonMethods;
 
-public class LoginSteps {
+public class LoginSteps extends CommonMethods {
 
-    public static WebDriver driver;
+    //public static WebDriver driver;
 
     @Given("user is navigated to HRMS application")
     public void user_is_navigated_to_hrms_application() throws InterruptedException {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.get("http://hrm.syntaxtechs.net/humanresources/symfony/web/index.php/auth/login");
-        Thread.sleep(2000);
+       // WebDriverManager.chromedriver().setup();
+       // driver = new ChromeDriver();
+       // driver.get("http://hrm.syntaxtechs.net/humanresources/symfony/web/index.php/auth/login");
+       // Thread.sleep(2000);
+        openBrowserAndLaunchApplication();
     }
 
     @When("user enters valid admin username and password")
